5bb4ce1e73dc6983315977d6fff8d04d962fe306
Joar Esteban Buitrago Carrillo <jebuitragoc@unal.edu.co>
2020-09-05 01:48:25 -0500

    Add slides and examples

---
+++ examples/basic.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+size_t foo_len(const char *s) {
+  return strlen(s);
+}
+
+int main() {
+  const char *a = NULL;
+
+  printf("size of a = %lu\n", foo_len(a));
+
+  return 0;
+}
